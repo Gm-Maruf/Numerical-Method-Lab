3c213eafae8f3f2d1fe722a58bc558b47bb6b71c
Md. Golam Maula Maruf <37345164+Gm-Maruf@users.noreply.github.com>
2018-11-30 00:50:58 +0600

    Operation with False Position Method

---
+++ FalsePositionMethod.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+class FalsePosition{
+private : double eps;
+private : double a,b,root;
+public : FalsePosition(){
+a = 0, b = 0, root = -1, eps = 0.0000000001;
+}
+public : double f(double x){
+return x*x*x + x*x + 3;
+}
+public : double getNext(double x0, double x1){
+return x0 +(f(x0) * (x1 - x0)) / (f(x0) - f(x1));
+}
+public : void findInterval(){
+do{
+a--, b++;
+}while(f(a) * f(b) > 0);
+if(f(b) < 0)
+swap(a,b);
+return;
+}
+public : double getRoot(){
+return root;
+}
+public : void findRoot(){
+findInterval();
+double mid = getNext(a,b);
+while(abs(f(b) - f(a)) > eps){
+if(f(mid) > 0)
+b = mid;
+else
+a = mid;
+mid = getNext(a,b);
+}
+root = mid;
+}
+};
+int main(){
+FalsePosition falsePosition;
+falsePosition.findRoot();
+printf("The root of the given equation : %0.10lf\n", falsePosition.getRoot());
+}
+
